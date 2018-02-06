a43c109a48d33d237ac1e5b5f5722151680b2ee9
jesser360 <jesser360@gmail.com>
2018-02-06 13:53:32 -0800

    email settings more

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,15 +62,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "c360_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-   address: 'smtp.mail.com',
-   port: 587,
-   domain: 'google.com',
-   user_name: 'c360supply',
-   password: '36093609',
-   authentication: 'plain',
-   enable_starttls_auto: true }
+  config.action_mailer.default_url_options = { :host => 'https://cbd360.herokuapp.com/' }
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings =
+  {
+
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com', #you can also use google.com
+    :authentication     => :plain,
+    :user_name          => 'c360supply@gmail.com',
+    :password           => '36093609'
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
