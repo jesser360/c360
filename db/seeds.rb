68c8eb0102a6d8c8fddf7c23a388a638f048af2c
jesse <jesser360@gmail.com>
2018-07-18 20:30:04 -0700

    reformat store into cards all done

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,20 @@
   buyer = User.create({email:'buyer@yahoo.com',password:'123',is_vendor:false,email_confirmed:true,zipcode:94618})
 
 
-  item = Item.create({item_name: "CBD Oil",user: seller, description: "Headache and body pain relief", rating: 0, rating_count: 0})
-  item.avatar = File.open('/Users/jesserosenbloom/code/c360/app/assets/images/cbd_oil.png')
-  item.save!
+  item1 = Item.create({item_name: "CBD Oil",user: seller, description: "Headache and body pain relief", rating: 10, rating_count: 2})
+  item1.avatar = File.open('/Users/jesse/code/c360/app/assets/images/cbd_oil.png')
+  item1.save!
+  item2 = Item.create({item_name: "CBD Concentrate Oil",user: seller, description: "Great for body aches and feeling good", rating: 8, rating_count: 2})
+  item2.avatar = File.open('/Users/jesse/code/c360/app/assets/images/cbd-oil2.png')
+  item2.save!
+  item3 = Item.create({item_name: "Isolate",user: seller, description: "Pure Isolate with only CBD ingredients", rating: 7, rating_count: 2})
+  item3.avatar = File.open('/Users/jesse/code/c360/app/assets/images/isolate.png')
+  item3.save!
+  item4 = Item.create({item_name: "Distillate",user: seller, description: "CBD Distillate made from high grade Hemp plants", rating: 3, rating_count: 1})
+  item4.avatar = File.open('/Users/jesse/code/c360/app/assets/images/distillate.png')
+  item4.save!
 
-  batch = BulkOrder.create({percent_filled: 0,max_amount: 400, expire_date: (Time.now + 7.days), completed: false, item: item, market_price: 12, wholesale_price: 10, item_name: "CBD Oil", description: "Headache and body pain relief", published: true, buyer_count: 0, token: "GMBMLCCyFv5fUX6qbNNYhpdU"})
+  batch = BulkOrder.create({percent_filled: 0,max_amount: 400, expire_date: (Time.now + 7.days), completed: false, item: item1, market_price: 11, wholesale_price: 7, published: true, buyer_count: 0, token: "GMBMLCCyFv5fUX6qbNNYhpdU"})
+  batch = BulkOrder.create({percent_filled: 0,max_amount: 400, expire_date: (Time.now + 7.days), completed: false, item: item2, market_price: 12, wholesale_price: 10, published: true, buyer_count: 0, token: "24bMtDf8g2hAVJAxpHpEW0fT"})
+  batch = BulkOrder.create({percent_filled: 0,max_amount: 400, expire_date: (Time.now + 7.days), completed: false, item: item3, market_price: 15, wholesale_price: 11, published: true, buyer_count: 0, token: "pHpEW0fTpHpEW0fTpHpEW0fT"})
+  batch = BulkOrder.create({percent_filled: 0,max_amount: 400, expire_date: (Time.now + 7.days), completed: false, item: item4, market_price: 18, wholesale_price: 16, published: true, buyer_count: 0, token: "jcKp+8ENjcKp+8ENjcKp+8EN"})
